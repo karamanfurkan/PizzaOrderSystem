751f3e38a9534b1e6af1624b58b779db5bf08e22
furkan karaman <b.furkankaraman@gmail.com>
2023-03-12 23:02:07 +0300

    Tarih Saat bilgisi eklendi

--- ordermain.py
+++ ordermain.py
@@ -1,7 +1,9 @@
+import datetime
+
 # Menü ve fiyatları
 menu = {1: "Klasik", 2: "Margarita", 3: "TürkPizza", 4: "Sade Pizza"}
 menu_prices = {1: 20, 2: 25, 3: 30, 4: 15}
-sauces = {11: "Zeytin", 12: "Mantar", 13: "Ekstra Kaşar Peyniri", 14: "Et", 15: "Soğan", 16: "Mısır"}
+sauces = {11: "Zeytin", 12: "Mantarlar", 13: "Keçi Peyniri", 14: "Et", 15: "Soğan", 16: "Mısır"}
 sauce_prices = {11: 5, 12: 4, 13: 6, 14: 8, 15: 3, 16: 2}
 
 # Sipariş bilgilerini
@@ -23,8 +25,9 @@ name = input("Lütfen adınızı ve soyadınızı girin: ")
 credit_card = input("Lütfen kredi kartı numaranızı girin: ")
 
 # Sipariş bilgilerini Orders_Database.csv dosyasına kaydetme yeri
+now = datetime.datetime.now()
 with open("Orders_Database.csv", "a") as file:
-    file.write(f"{name},{menu[pizza_choice]},{sauces[sauce_choice]},{total_price} TL ,{credit_card}\n")
+    file.write(f"{name},{menu[pizza_choice]},{sauces[sauce_choice]},{total_price} TL ,{credit_card},{now}\n")
 
 # Kullanıcıya sipariş onayı verin
 print("Siparişiniz başarıyla alındı!")
